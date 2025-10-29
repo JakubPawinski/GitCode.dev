e1e750c84ca0fa176308cd98bfc38090e2a8c1bb
Adrian Dobkowski <adrian.dobkowski2c@gmail.com>
2025-10-29 15:42:38 +0100

    project init / eslint,prettier configuration

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+
+import prettierPlugin from 'eslint-plugin-prettier/recommended';
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  prettierPlugin,
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+  {
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          trailingComma: 'all',
+          printWidth: 100,
+          tabWidth: 2,
+          semi: true,
+        },
+      ],
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+  },
+]);
+
+export default eslintConfig;
