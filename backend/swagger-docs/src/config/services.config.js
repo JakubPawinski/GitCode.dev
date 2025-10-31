289bacef766403d5febd956df51bb03feab81f7f
Jakub Pawiński <j.pawinski.164@studms.ug.edu.pl>
2025-10-31 22:11:42 +0100

    feat(swagger-docs): add swagger docs service for accumulating all microservices swagger docs

---
+++ backend/swagger-docs/src/config/services.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+	port: process.env.SWAGGER_DOCS_PORT || 4050,
+	buildDelay: 4000, 
+
+	apiGatewaySpec: {
+		openapi: '3.0.0',
+		info: {
+			title: 'GitCode API Gateway',
+			version: '1.0.0',
+			description: 'API documentation for all microservices',
+		},
+		servers: [
+			{ url: 'http://localhost:8080', description: 'API Gateway Server' },
+		],
+	},
+
+	services: [
+		{
+			name: 'Auth Service',
+			url: `${process.env.AUTH_SERVICE_URL}/docs-json`,
+			pathPrefix: '/api',
+		},
+	],
+};
