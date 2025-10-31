fcd74cbc04273a64817a266625ff93ccca43d03f
Jakub Pawiński <j.pawinski.164@studms.ug.edu.pl>
2025-10-31 22:08:07 +0100

    feat(api-gateway): add proxy implementation and /auth endpoint for testing

--- backend/auth-service/src/app.controller.ts
+++ backend/auth-service/src/app.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller()
+@ApiTags('Auth Service')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get Auth Service status' })
   getHello(): string {
     return this.appService.getHello();
   }
