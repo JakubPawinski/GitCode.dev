9139fab07b30e17430026906e2a338f123be689e
Jakub Pawiński <j.pawinski.164@studms.ug.edu.pl>
2025-11-22 22:12:24 +0100

    fix(auth-service): add AuthModule to UsersModule imports

--- backend/auth-service/src/users/users.module.ts
+++ backend/auth-service/src/users/users.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './providers/users.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
 })
 export class UsersModule {}
