f608c082f79cb4be0fb30de5ebc4ffa8fd5aa629
Jakub Pawiński <j.pawinski.164@studms.ug.edu.pl>
2025-11-29 15:30:13 +0100

    refactor(backend): remove unused services after migrating to apps

--- backend/auth-service/src/auth/enums/user-status.enum.ts
+++
@@ -1,6 +0,0 @@
-export enum UserStatus {
-  ACTIVE = 'ACTIVE',
-  INACTIVE = 'INACTIVE',
-  BANNED = 'BANNED',
-  DELETED = 'DELETED',
-}
--- backend/auth-service/src/users/dtos/get-public-profile.dto.ts
+++
@@ -1,8 +0,0 @@
-import { OmitType } from '@nestjs/swagger';
-import { GetProfileDto } from './get-profile.dto';
-
-export class GetPublicProfileDto extends OmitType(GetProfileDto, [
-  'email',
-  'emailVerified',
-  'roles',
-] as const) {}
