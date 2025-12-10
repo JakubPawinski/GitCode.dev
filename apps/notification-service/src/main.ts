f674f6681adfbc888041766696a165c02225df1b
Jakub Pawiński <j.pawinski.164@studms.ug.edu.pl>
2025-12-10 23:33:03 +0100

    chore(notification-service): implement basic initialization for Prisma and Redis services with RabbitMQ integration

--- apps/notification-service/src/main.ts
+++ apps/notification-service/src/main.ts
@@ -2,20 +2,26 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  */
-
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+
+  app.connectMicroservice<any>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [process.env.RABBITMQ_URL || 'amqp://guest:guest@localhost:5672'],
+      queue: process.env.NOTIFICATION_QUEUE_NAME || 'notification_queue',
+      queueOptions: {
+        durable: true,
+      },
+    },
+  });
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
-  );
 }
 
 bootstrap();
