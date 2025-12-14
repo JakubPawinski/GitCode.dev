db25613a3b042522a0fc7a926a7252d8270acff8
Jakub Pakula <j.pakula.103@studms.ug.edu.pl>
2025-12-14 13:21:20 +0100

    refactor(packages): fix export to be index instead of api-resposnse.dto, remove unnecesary pagination form api-response, export new pagiantion.dto

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,4 +1,4 @@
 export * from './lib/common.module';
-export * from './lib/dtos/api-response.dto.ts';
+export * from './lib/dtos/index.ts';
 export * from './lib/filters/index.ts';
 export * from './lib/interceptors/index.ts';
