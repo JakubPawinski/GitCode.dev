af790ba4272b90bd43926dde54924083484e4e81
Jakub Pakula <j.pakula.103@studms.ug.edu.pl>
2026-02-03 19:08:34 +0100

    feat(github-service): add GITHUB_QUEUE_NAME to .env.example and update RabbitMQ config to use it

--- apps/github-service/src/app/config/rabbitmq.config.ts
+++ apps/github-service/src/app/config/rabbitmq.config.ts
@@ -2,11 +2,11 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('rabbitmq', () => ({
   url: process.env.RABBITMQ_URL || 'amqp://guest:guest@localhost:5672',
-  queueName: process.env.NOTIFICATION_QUEUE_NAME || 'notification_queue',
+  queueName: process.env.GITHUB_QUEUE_NAME || 'github_queue',
   exchangeName: process.env.RABBITMQ_EXCHANGE_NAME || 'gitcode_exchange',
 }));
 
 export const RABBIT_CONFIG = {
   EXCHANGE: process.env.RABBITMQ_EXCHANGE_NAME || 'gitcode_exchange',
-  QUEUE: process.env.NOTIFICATION_QUEUE_NAME || 'notification_queue',
-}
+  QUEUE: process.env.GITHUB_QUEUE_NAME || 'github_queue',
+};
