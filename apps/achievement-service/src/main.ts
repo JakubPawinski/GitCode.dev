a4beee57c06fa1857171c9189e020bd6862b7de4
Jakub Pawiński <j.pawinski.164@studms.ug.edu.pl>
2026-03-09 18:38:34 +0100

    feat(achievement-service): configure global validation and exception handling in main.ts

--- apps/achievement-service/src/main.ts
+++ apps/achievement-service/src/main.ts
@@ -6,15 +6,31 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from '@gitcode/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  // Global exception filter
+  app.useGlobalFilters(new HttpExceptionFilter('notification-service'));
+
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  });
+  const port = process.env.ACHIEVEMENT_PORT;
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+    `🚀 Application is running on: http://localhost:${port}`,
   );
 }
 
