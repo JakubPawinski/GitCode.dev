220b660643a99223e761aff12472bbb8781b7de2
Jakub Pawiński <j.pawinski.164@studms.ug.edu.pl>
2026-03-10 11:46:17 +0100

    feat(achievement-service): inject auth module into achievement service

--- apps/achievement-service/src/app/app.module.ts
+++ apps/achievement-service/src/app/app.module.ts
@@ -7,7 +7,9 @@ import { ConfigModule } from '@nestjs/config';
 import envValidation from './config/env.validation';
 import rabbitmqConfig from './config/rabbitmq.config';
 import appConfig from './config/app.config';
+import jwtConfig from './config/jwt.config';
 import { AchievementModule } from '../achievement/achievement.module';
+import { GitCodeAuthModule } from '@gitcode/auth';
 
 @Module({
   imports: [
@@ -15,10 +17,11 @@ import { AchievementModule } from '../achievement/achievement.module';
     MessagingModule.forRoot([process.env.RABBITMQ_URL]),
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [appConfig, rabbitmqConfig],
+      load: [appConfig, rabbitmqConfig, jwtConfig],
       validationSchema: envValidation,
     }),
     AchievementModule,
+    GitCodeAuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
